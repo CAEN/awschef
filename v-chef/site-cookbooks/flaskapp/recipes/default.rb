a5fa4d76e5a20cae21237871a3c09bc0b904c37f
Andrew Caird <acaird@umich.edu>
2015-02-10 09:52:29 -0500

    added conf_dir variable and used it; removed unneeded symlink
    The 'conf_dir' setting was taken from the apache2 default recipe; I
    tried including that file so conf_dir was only set in one place, but
    there isn't an 'ubuntu' case in that file, so it didn't work, and I
    thought it was better to put this here than to edit the
    apache2::default.rb file

--- v-chef/site-cookbooks/flaskapp/recipes/default.rb
+++ v-chef/site-cookbooks/flaskapp/recipes/default.rb
@@ -1,10 +1,8 @@
 package "git"
 
-cookbook_file "/etc/apache2/conf-available/flask-virthost.conf" do
+conf_dir = node[:apache][:conf_enabled_dir] || "#{node[:apache][:dir]}/conf.d"
+
+cookbook_file "#{conf_dir}/flask-virthost.conf" do
 	source "flask-virthost.conf"
 	mode "0644"
-end
-
-link "/etc/apache2/conf-enabled/flask-virthost.conf" do
-	to "../conf-available/flask-virthost.conf" 
 end

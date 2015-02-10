2ca68414d99c74f8ad4fab1a83372767a7340557
Andrew Caird <acaird@umich.edu>
2015-02-10 10:20:17 -0500

    added python-setuptools to get easy_install, used that to install flask and plotly

--- v-chef/site-cookbooks/flaskapp/recipes/default.rb
+++ v-chef/site-cookbooks/flaskapp/recipes/default.rb
@@ -1,6 +1,17 @@
 package "git"
+package "python-setuptools"  # for easy_install
 
 conf_dir = node[:apache][:conf_enabled_dir] || "#{node[:apache][:dir]}/conf.d"
+
+execute "install_flask" do
+	command "easy_install flask"
+	user "root"
+end
+
+execute "install_plotly" do
+	command "easy_install plotly"
+	user "root"
+end
 
 cookbook_file "#{conf_dir}/flask-virthost.conf" do
 	source "flask-virthost.conf"

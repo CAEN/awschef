9c19a096ae29f613d4c7e6634f04415fea4c3051
Andrew Caird <acaird@umich.edu>
2015-02-10 10:24:47 -0500

    added some mkdirs to set up for app install

--- v-chef/site-cookbooks/flaskapp/recipes/default.rb
+++ v-chef/site-cookbooks/flaskapp/recipes/default.rb
@@ -3,6 +3,21 @@ package "python-setuptools"  # for easy_install
 
 conf_dir = node[:apache][:conf_enabled_dir] || "#{node[:apache][:dir]}/conf.d"
 
+# make a home for our flask application
+directory "/var/www/flask/appstack" do
+	  owner "ubuntu"
+	  group "root"
+	  mode "0755"
+	  action :create
+end
+
+# make a home for our git pull
+directory "/tmp/.appstack_deploy/.ssh" do
+	  owner "ubuntu"
+	  recursive true
+end
+
+# install flask with easy_install
 execute "install_flask" do
 	command "easy_install flask"
 	user "root"
